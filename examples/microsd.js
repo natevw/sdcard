176c638b784479c0a63c4c3238219aa00cb64996
Frijol <ifoundthemeaningoflife@gmail.com>
2014-06-04 00:38:54 -0700

    added example for FRE

---
+++ examples/microsd.js
@@ -0,0 +1,21 @@
+/*********************************************
+This MicroSD card example writes a text file
+to the sd card, then reads the file to the
+console.
+*********************************************/
+
+var tessel = require('tessel');
+var sdcard = require('sdcard').use(tessel.port['A']);
+
+sdcard.on('ready', function() {
+  sdcard.getFilesystems(function(err, fss) {
+    var fat = fss[0];
+    console.log('Writing...');
+    fat.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
+      console.log('Write complete. Reading...');
+      fat.readFile('someFile.txt', function(err, data) {
+        console.log('Read:\n', data.toString());
+      });
+    });
+  });
+});
