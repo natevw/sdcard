150e58103756b69b1a47849b74daa7c8afb5680a
Nathan Vander Wilt <nate@calftrail.com>
2014-06-04 14:37:33 -0700

    quick tweak to microsd example to use as a performance benchmark of sorts

---
+++ examples/timed_microsd.js
@@ -0,0 +1,23 @@
+/*********************************************
+This MicroSD card example writes a text file
+to the sd card, then reads the file to the
+console.
+*********************************************/
+
+var tessel = require('tessel');
+var sdcard = require('../').use(tessel.port['A']);
+
+sdcard.on('ready', function() {
+  sdcard.getFilesystems(function(err, fss) {
+    var fs = fss[0],
+        start = Date.now();
+    console.log('Writing...');
+    fs.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
+      console.log('Write complete. Reading...');
+      fs.readFile('someFile.txt', function(err, data) {
+        console.log('Read:\n', data.toString());
+        console.log("TIME:", Date.now()-start);
+      });
+    });
+  });
+});
