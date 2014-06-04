9819d6f65ed4b5929fb2f0a98a5d2dd0e59e1867
Nathan Vander Wilt <nate@calftrail.com>
2014-06-04 14:48:47 -0700

    add setup time to time test script too

--- examples/timed_microsd.js
+++ examples/timed_microsd.js
@@ -4,6 +4,8 @@ to the sd card, then reads the file to the
 console.
 *********************************************/
 
+var _start = Date.now();
+
 var tessel = require('tessel');
 var sdcard = require('../').use(tessel.port['A']);
 
@@ -11,12 +13,13 @@ sdcard.on('ready', function() {
   sdcard.getFilesystems(function(err, fss) {
     var fs = fss[0],
         start = Date.now();
+    console.log("_time_", start - _start);
     console.log('Writing...');
     fs.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
       console.log('Write complete. Reading...');
       fs.readFile('someFile.txt', function(err, data) {
         console.log('Read:\n', data.toString());
-        console.log("TIME:", Date.now()-start);
+        console.log(":TIME:", Date.now()-start);
       });
     });
   });
