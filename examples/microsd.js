77c6a0a441f5791a25bd6e5c4c653d134e9ba16f
Frijol <ifoundthemeaningoflife@gmail.com>
2014-06-04 01:24:49 -0700

    changed fat to fs

--- examples/microsd.js
+++ examples/microsd.js
@@ -9,11 +9,11 @@ var sdcard = require('../').use(tessel.port['A']);
 
 sdcard.on('ready', function() {
   sdcard.getFilesystems(function(err, fss) {
-    var fat = fss[0];
+    var fs = fss[0];
     console.log('Writing...');
-    fat.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
+    fs.writeFile('someFile.txt', 'Hey Tessel SDCard!', function(err) {
       console.log('Write complete. Reading...');
-      fat.readFile('someFile.txt', function(err, data) {
+      fs.readFile('someFile.txt', function(err, data) {
         console.log('Read:\n', data.toString());
       });
     });
